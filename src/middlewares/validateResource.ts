04209329f2adfcc824222bcaace6c41f42e2ed1f
Akash187 <kumarakash007@gmail.com>
2022-02-23 03:31:34 +0530

    refactor: added logger for validateResource

--- src/middlewares/validateResource.ts
+++ src/middlewares/validateResource.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { AnyZodObject } from 'zod'
+import { logger } from '../utils/logger'
 
 export const validateResource =
 	(schema: AnyZodObject) =>
@@ -12,6 +13,7 @@ export const validateResource =
 			})
 			next()
 		} catch (e: any) {
+			logger.error('validate Resource Failed.')
 			return res.status(400).send(e.errors)
 		}
 	}
