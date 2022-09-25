6fc4413100542f20493e93f2f48c2db4dfba5451
Akash187 <kumarakash007@gmail.com>
2022-09-25 23:02:55 +0530

    fix: minor update in projection string

--- src/utils/projection.ts
+++ src/utils/projection.ts
@@ -2,6 +2,6 @@ export const UserProjection =
 	'name userName bio photo followerCount followingCount'
 
 export const BlogProjection =
-	'publishedTitle publishedContent subTitle slug previewImage tags readTime publishedAt userId user clapsCount responsesCount status isPublished'
+	'publishedTitle publishedContent subTitle slug previewImage tags readTime publishedAt userId clapsCount responsesCount status isPublished'
 
 export const ClapProjection = 'claps userId postId relatedTo'
